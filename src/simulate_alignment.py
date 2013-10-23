7a7050b03e12e48085d04373af82392d8dd338c1
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2013-10-23 15:09:02 -0700

    added main script

---
+++ src/simulate_alignment.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+''' simulate_alignment.py -- Generate a hypothetical protein alignment.
+
+	Learns parameters from a given protein alignment and uses them
+	to simulate hypothetical alignment of *the same length*.
+
+	*Be careful with gaps*
+
+'''
+
+__author__ = 'Aram.Avila-Herrera'
+__email__ = 'Aram.Avila-Herrera@ucsf.edu'
+
+import sys
+import getopt
+from os.path import exists
+
+def get_cmd_options(args):
+	''' Parse command line for options and return a dict.
+
+		using getopt for compatibility
+
+	'''
+	
+	usage = (
+				'usage: %s [-h | [ --tree tree_file ] \n'+\
+				'				[ --hmmer_db pfam.hmm ]\n'+\
+				'				[ --outdir outdir ] input_aln.phy'
+			) % sys.argv[0]
+	
+	try:
+		optlist, args = getopt.getopt(args, 'ht:d:o:',
+							['help', 'tree=', 'hmmer_db=',
+							'outdir='])
+	except getopt.GetoptError as err:
+		print >>sys.stderr, 'Error: %s' % err
+		sys.exit(usage)
+	
+	options = dict()
+	
+	# read command line options
+	for opt, val in optlist:
+		if opt in ('-h', '--help'):
+			sys.exit(usage)
+		if opt in ('-o', '--outdir'):
+			options['outdir'] = val
+		if opt in ('-d', '--hmmer_db'):
+			options['hmmer_db'] = val
+		if opt in ('t', '--tree'):
+			options['tree'] = val
+
+	if len(args) > 0:
+		if exists(args[0]):
+			options['aln_fn'] = args[0]
+		else:
+			print >>sys.stderr, '%s: file %s not found' % (sys.argv[0], args[0])
+	else:
+		print >>sys.stderr, '%s: alignment file not specified' % sys.argv[0]
+	
+	print 'running with options:'
+	[ sys.stdout.write('\t%s: %s\n'%(opt, val))
+				for opt, val in sorted(options.iteritems()) ]
+		
+	return options
+	
+def main(options):
+	print 'in main()'
+
+if __name__ == '__main__':
+	options = get_cmd_options(sys.argv[1:])
+	main(options)
+
+
