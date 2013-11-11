b47d4ab693c4269f60cd539f45db5d73cbe6e801
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2013-11-11 15:42:59 -0800

    removed redundancies between test.sh and runAliSim.sh

--- test/test.sh
+++ test/test.sh
@@ -4,19 +4,20 @@
 # Input, output, and job_name are fixed, but you can still
 # pass extra options like '--skip_hmmer' or '--help' when calling
  
+### This is now included in ../runSimAli.sh helper script ###
+# #alias revolver=java -cp /path/to/revolver/ revolver
+# export __REVOLVER="revolver"
+# export __SRC_PATH="/home/aram/dev-src/simulate_tools"
+#############################################################
 
-# alias revolver=java -cp /path/to/revolver/ revolver
-export __REVOLVER="revolver"
-export __SRC_PATH="/home/aram/dev-src/simulate_tools"
 
-TESTDIR="${__SRC_PATH}/test"
+#TESTDIR="${__SRC_PATH}/test"
+
+TESTDIR=`pwd`
 
 ALN="${TESTDIR}/A3G.phy"
 TRE="${TESTDIR}/A3G.tre"
 OUTDIR="${TESTDIR}"
 
-python ${__SRC_PATH}/src/simulate_alignment.py --num_sims 5 --outdir ${OUTDIR} --tree ${TRE} ${@} test_job ${ALN}
-
-
-
-
+#python ${__SRC_PATH}/src/simulate_alignment.py --num_sims 5 --outdir ${OUTDIR} --tree ${TRE} ${@} test_job ${ALN}
+bash ../runSimAli.sh --num_sims 5 --outdir ${OUTDIR} --tree ${TRE} ${@} test_job ${ALN}
